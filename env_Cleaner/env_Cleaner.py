fc2eeda2d3cd4418168a1814288a97483f9678de
Tristan Tisch <tristan.tisch@pickware.de>
2024-07-31 14:48:49 +0200

    Change observations to be different for each agent and refactoring

--- env_Cleaner/env_Cleaner.py
+++ env_Cleaner/env_Cleaner.py
@@ -1,7 +1,8 @@
-import numpy as np
-import maze
-import random
 import cv2
+import numpy as np
+
+from LtoS.environments.grid_environments.env_Cleaner import maze
+
 
 class EnvCleaner(object):
     def __init__(self, N_agent, map_size, seed):
@@ -34,41 +35,39 @@ class EnvCleaner(object):
         return grid_map
 
     def step(self, action_list):
-        reward = 0
+        # TODO: change to per-agent rewards
+        reward = np.zeros((self.N_agent,))
         for i in range(len(action_list)):
-            if action_list[i] == 0:     # up
+            if action_list[i] == 0:  # up
                 if self.occupancy[self.agt_pos_list[i][0] - 1][self.agt_pos_list[i][1]] != 1:  # if can move
                     self.agt_pos_list[i][0] = self.agt_pos_list[i][0] - 1
-            if action_list[i] == 1:     # down
+            if action_list[i] == 1:  # down
                 if self.occupancy[self.agt_pos_list[i][0] + 1][self.agt_pos_list[i][1]] != 1:  # if can move
                     self.agt_pos_list[i][0] = self.agt_pos_list[i][0] + 1
-            if action_list[i] == 2:     # left
+            if action_list[i] == 2:  # left
                 if self.occupancy[self.agt_pos_list[i][0]][self.agt_pos_list[i][1] - 1] != 1:  # if can move
                     self.agt_pos_list[i][1] = self.agt_pos_list[i][1] - 1
-            if action_list[i] == 3:     # right
+            if action_list[i] == 3:  # right
                 if self.occupancy[self.agt_pos_list[i][0]][self.agt_pos_list[i][1] + 1] != 1:  # if can move
                     self.agt_pos_list[i][1] = self.agt_pos_list[i][1] + 1
-            if self.occupancy[self.agt_pos_list[i][0]][self.agt_pos_list[i][1]] == 2:   # if the spot is dirty
+            if self.occupancy[self.agt_pos_list[i][0]][self.agt_pos_list[i][1]] == 2:  # if the spot is dirty
                 self.occupancy[self.agt_pos_list[i][0]][self.agt_pos_list[i][1]] = 0
-                reward = reward + 1
-        return reward
+                reward[i] = reward[i] + 1
+        return self.get_global_obs(), reward, np.all(self.occupancy != 2), {}
 
     def get_global_obs(self):
-        obs = np.zeros((self.map_size, self.map_size, 3))
-        for i in range(self.map_size):
-            for j in range(self.map_size):
-                if self.occupancy[i, j] == 0:
-                    obs[i, j, 0] = 1.0
-                    obs[i, j, 1] = 1.0
-                    obs[i, j, 2] = 1.0
-                if self.occupancy[i, j] == 2:
-                    obs[i, j, 0] = 0.0
-                    obs[i, j, 1] = 1.0
-                    obs[i, j, 2] = 0.0
+        # 0: clean, 1: wall, 2: dirty, 3: agent
+        # clean/dirty/wall, agents
+        obs = np.zeros((self.N_agent, 2, self.map_size, self.map_size))
         for i in range(self.N_agent):
-            obs[self.agt_pos_list[i][0], self.agt_pos_list[i][1], 0] = 1.0
-            obs[self.agt_pos_list[i][0], self.agt_pos_list[i][1], 1] = 0.0
-            obs[self.agt_pos_list[i][0], self.agt_pos_list[i][1], 2] = 0.0
+            obs[i, 0, :, :] = self.occupancy  # set clean/dirty/wall
+            obs[i, 0][obs[i, 0] == 3] = 0  # set all agent positions to clean
+            for i_agent, (pos_x, pos_y) in enumerate(self.agt_pos_list):
+                obs[i, 1, pos_x, pos_y] = i_agent + 1
+            obs[i, 1, self.agt_pos_list[i][0], self.agt_pos_list[i][1]] = i + 1  # make sure that the agents position is always "on top"
+        obs = obs.reshape((self.N_agent, -1))
+        agent_index = np.array([[i + 1] for i in range(self.N_agent)])
+        obs = np.concatenate((obs, agent_index), axis=1)
         return obs
 
     def reset(self):
@@ -80,7 +79,7 @@ class EnvCleaner(object):
     def render(self):
         obs = self.get_global_obs()
         enlarge = 5
-        new_obs = np.ones((self.map_size*enlarge, self.map_size*enlarge, 3))
+        new_obs = np.ones((self.map_size * enlarge, self.map_size * enlarge, 3))
         for i in range(self.map_size):
             for j in range(self.map_size):
                 if obs[i][j][0] == 0.0 and obs[i][j][1] == 0.0 and obs[i][j][2] == 0.0:
